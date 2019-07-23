6d012ff24f56cc6768411aa0e97f187dc25bdeda
edi4404 <ediboghian@yahoo.com>
2019-07-23 22:43:55 +0300

    sa

--- src/App.js
+++ src/App.js
@@ -5,13 +5,9 @@ export default class App extends Component {
   render() {
     return (
       <div className='App'>
-<<<<<<< HEAD
-        hello
-        <Home />
-=======
         hello!!
->>>>>>> 0e90101a1dcfba3be92177fa98f4be2ffc17f3b3
-      </div>
+        <Home />
+      </div >
     )
   }
 }
