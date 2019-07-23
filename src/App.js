0c0146a154d05e90e7859a7afb7d039ed5991b8b
edi4404 <ediboghian@yahoo.com>
2019-07-23 21:51:27 +0300

    erase null

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { connect } from "react-redux";
-import BaseRouter from "./routes";
-import * as actions from "./store/actions/auth";
-import "semantic-ui-css/semantic.min.css";
-import CustomLayout from "./containers/Layout";
+import React, { Component } from "react"
 
-class App extends Component {
-  componentDidMount() {
-    this.props.onTryAutoSignup();
-  }
-
+export default class App extends Component {
   render() {
     return (
-      <Router>
-        <CustomLayout {...this.props}>
-          <BaseRouter />
-        </CustomLayout>
-      </Router>
-    );
+      <div className='App'>
+        hello
+      </div>
+    )
   }
 }
-
-const mapStateToProps = state => {
-  return {
-    isAuthenticated: state.auth.token !== null
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onTryAutoSignup: () => dispatch(actions.authCheckState())
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import { createStore, compose, applyMiddleware, combineReducers } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-
-import authReducer from "./store/reducers/auth";
-
-const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const rootReducer = combineReducers({
-  auth: authReducer
-});
-
-const store = createStore(rootReducer, composeEnhances(applyMiddleware(thunk)));
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
 
 const app = (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+  <App />
+)
 
-ReactDOM.render(app, document.getElementById("root"));
-registerServiceWorker();
+ReactDOM.render(app, document.getElementById("root"))
